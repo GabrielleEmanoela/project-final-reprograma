746fececa2a72d4be20f50bcef66c58094622629
Gabrielle Oliveira <biela.a09@gmail.com>
2021-07-06 17:53:53 -0300

    Create Card in Dashboard.

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Header from '~/components/Header';
 import imgDashBoard from '~/assets/dashboard.svg';
 import './styles.css';
+import Card from '~/components/Card';
 import icon from '~/assets/icon.png';
 
 function Main() {
+  function teste() {
+    return console.tron.log('teste');
+  }
   return (
     <>
       <Header />
@@ -14,11 +18,15 @@ function Main() {
         </div>
         <div className="containerTitle">
           <img src={icon} alt="logoLove" />
-
           <h1 className="title">SURPREENDA SEU AMOR!</h1>
           <p className="msg">Por aqui envie incriveis mensagens.</p>
+          <button type="button" className="buttonConfirmation" onClick={teste}>
+            <p className="pButton">Vamos lá!</p>
+          </button>
         </div>
-
+        <div>
+          <Card />
+        </div>
         <div />
       </div>
     </>
