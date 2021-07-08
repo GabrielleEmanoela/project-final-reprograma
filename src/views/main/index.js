ca2c4b0cb605064e8ce62f2533929777c9703c36
Gabrielle Oliveira <biela.a09@gmail.com>
2021-07-07 23:23:22 -0300

    Aplicantion configuration.

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,32 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '~/components/Header';
-import imgDashBoard from '~/assets/dashboard.svg';
 import './styles.css';
 import Card from '~/components/Card';
 import icon from '~/assets/icon.png';
+import Footer from '~/components/Footer';
 
 function Main() {
   function teste() {
-    return console.tron.log('teste');
+    return (
+      <Link className="link" to="/explore">
+        Explore
+      </Link>
+    );
   }
   return (
     <>
       <Header />
       <div>
         <div className="stylesImgDash">
-          <img src={imgDashBoard} alt="logoLove" width={350} />
+          <img
+            src="https://i.imgur.com/2G4xjeF.png"
+            alt="logoLove"
+            width={400}
+          />
         </div>
         <div className="containerTitle">
           <img src={icon} alt="logoLove" />
-          <h1 className="title">SURPREENDA SEU AMOR!</h1>
-          <p className="msg">Por aqui envie incriveis mensagens.</p>
+          <h1 className="title"> Correio do Amor </h1>
+          <p className="msg">
+            Chegou a hora de usar nossa criatividade e enviar uma mensagem para
+            seu amigo, seu seguidor e praquele ou praquela crush que você sempre
+            quis enviar 😉
+          </p>
           <button type="button" className="buttonConfirmation" onClick={teste}>
             <p className="pButton">Vamos lá!</p>
           </button>
         </div>
-        <div>
-          <Card />
-        </div>
+        <div className="teste" />
+        <Card />
+        <Footer />
+
         <div />
       </div>
     </>
